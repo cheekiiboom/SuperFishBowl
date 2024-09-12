739ca9ccfec024b2439491189dea849d20e16209
Titus Whang <tituswhang0930@gmail.com>
2024-09-12 02:09:55 -0400

    Added additional functionalities for keys

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    public Material Material;
+    public GameObject Door;
+    public GameObject[] ObjectsToDestroy;
+    private Rigidbody rigidbody;
     private void OnTriggerEnter(Collider other)
     {
         if (other != null && other.tag == "Player")
+        {
+            rigidbody = Door.GetComponent<Rigidbody>();
+            rigidbody.isKinematic = false;
+            Door.GetComponent<MeshRenderer>().material = Material;
+
+            foreach (var obj in ObjectsToDestroy)
+                Destroy(obj);
+
             Destroy(gameObject);
+        }
     }
 }
