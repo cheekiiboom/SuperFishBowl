5657d15f9d57ba7adef0555b0d46a04ba9a4db8a
cheekiiboom <michaelsinger1000@gmail.com>
2024-09-15 19:34:53 -0400

    build version

--- Assets/Scripts/FishBowlWater.cs
+++ Assets/Scripts/FishBowlWater.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FishBowlWater : MonoBehaviour
 {
@@ -82,6 +83,7 @@ public class FishBowlWater : MonoBehaviour
             scaleFactor = 0;
             playerController.enabled = false;
             cameraController.enabled = false;
+            SceneManager.LoadScene(2); // death scene
         }
 
         foreach (var obj in water)
