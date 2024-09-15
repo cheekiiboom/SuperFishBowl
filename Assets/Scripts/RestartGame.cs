ca3f23fc062f5e612e24c817272b709334d14027
cheekiiboom <michaelsinger1000@gmail.com>
2024-09-15 19:06:58 -0400

    Death screen

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
